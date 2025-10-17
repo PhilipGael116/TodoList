c5ddbac90a1715d8396bb273c047a508ab6d0466
Philippe Gael <gaelngando@gmail.com>
2025-10-17 16:54:41 +0100

    Just finished with the Get Todos API fully working

--- backend/controllers/todoList.controllers.js
+++ backend/controllers/todoList.controllers.js
@@ -9,3 +9,16 @@ export const createTodo = async(req, res) => {
         res.status(500).json({message: error.message});
     }
 }
+
+export const getAllTodos = async(req, res) => {
+    try {
+        const todos = await TodoList.find().select("text completed");
+        if(!todos) {
+            return res.status(404).json({message: "No todos found"});
+        }
+        res.status(200).json(todos);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+}
--- backend/routes/todoList.routes.js
+++ backend/routes/todoList.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createTodo } from "../controllers/todoList.controllers.js"
+import { createTodo, getAllTodos } from "../controllers/todoList.controllers.js"
 
 const router = express.Router();
 
 router.post("/", createTodo);
+router.get("/", getAllTodos);
 
 export default router;
 
